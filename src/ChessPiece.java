69b7e7f488d8097bc43ce7bf2154f52f618b8b07
ANB <freizer1997@gmail.com>
2023-02-05 16:52:09 +0300

    Initial commit

---
+++ src/ChessPiece.java
@@ -0,0 +1,16 @@
+abstract public class ChessPiece {
+    String color;
+    boolean check = true;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    abstract String getColor();
+
+    abstract String getSymbol();
+
+    abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+
+}
